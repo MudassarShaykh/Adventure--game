25406ed7fa608648fe41c9e6fa8c8a961d1fd5a5
MudassarShaykh <161149123+MudassarShaykh@users.noreply.github.com>
2024-07-25 13:08:57 -0700

    first comment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#! usr/bin/env/ node
+#! /usr/bin/env node
 import inquirer from "inquirer";
 // ------------------------------------Game Variables------------------------------------
 let enemies = ["Zombie", "Skeleton", "Alien", "Dragon"];
